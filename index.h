0764409837d10213d833a89a0019f5353a986cb2
Võ Đức Phúc <duyvo1954@gmail.com>
2024-05-19 19:13:46 +0700

    index.h

--- index.h
+++ index.h
@@ -1,14 +1,12 @@
 #ifndef index_h
 #define index_h
 
-// define libraries //
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <time.h>
 
-// define macros
 #define FILE_DON_THUOC   "thuoc.txt"
 #define FILE_HOA_DON_NHAP "hoadonnhap.txt"
 #define FILE_HOA_DON_XUAT "hoadonxuat.txt"
@@ -22,142 +20,96 @@
 
 enum Prescription_properties {maThuoc, tenThuoc, thanhPhanChinh, ghiChuCuaDonThuoc, congDung};
 enum Bill_Properties {maHoaDon, ghiChuCuaHoaDon, maDonThuoc, soLo, soLuong};
-enum DateType {ngayTrenHoaDon = 0, Ngay_sx = 0, Ngay_het_han = 1};
+enum DateType {Ngay_sx, Ngay_het_han};
 enum PropertiesType {donThuoc, hoaDon};
 
-// define structures
 struct Date {
 	int day, month, year;
 	Date() : day(0), month(0), year(0) {} 
 	Date(int d, int m, int y) : day(d), month(m), year(y) {} 
 };
-struct Don_thuoc {
-	char *info[PRESCRIPTION_PROPERTIES];
-	Don_thuoc() { for(int i = 0; i < PRESCRIPTION_PROPERTIES; i++) { info[i] = strdup(""); } }
-};
+struct Don_thuoc {char *info[PRESCRIPTION_PROPERTIES];};
 struct Hoa_Don {
-	Date dates[1];
+	Date date;
 	char* info[BILL_PROPERTIES];
-	Hoa_Don() { 
-		dates[0] = Date();
-		for(int i = 0; i < BILL_PROPERTIES; i++) { info[i] = strdup(""); } 
-	}
 };
 struct Hoa_Don_Xuat {
 	Hoa_Don info;
 	char nguoi_mua[MAX_SIZE_STRING];
-	Hoa_Don_Xuat() { 
-		for(int i = 0; i < BILL_PROPERTIES; i++) { info.info[i] = strdup(""); } 
-	}
 };
 struct Hoa_Don_Nhap {
 	Hoa_Don	info;
 	Date dates[2];
 	char Congty_sx[MAX_SIZE_STRING];
-	Hoa_Don_Nhap() { 
-		dates[0] = dates[1] = Date();
-		for(int i = 0; i < BILL_PROPERTIES; i++) { info.info[i] = strdup(""); } 
-	}
 };
 struct Node {
 	void* data;
 	Node* next;
 	Node(void* newData) : data(newData), next(nullptr) {}
 };
-
-// define prototypes //
-void showNotification(const char* notification);
-void* readfile(FILE* file, const char* fileName);
-
-void getProperties(FILE* file, void* variable, const char* format, int vType, int pType);
-void getDate(FILE* file, const char* format, Date& variable);
-
-// Don thuoc //
-void* getPrescription(FILE* file);
-void printPrescription(Don_thuoc* value);
-// Hoa don //
-Hoa_Don getBill(FILE* file);
-void printBill(Hoa_Don value, const char* string);
-// Hoa don xuat //
-void* getExportingBill(FILE* file);
-void printExportingBill(Hoa_Don_Xuat* value);
-// Hoa don nhap //
-void* getImportingBill(FILE* file);
-void printImportingBill(Hoa_Don_Nhap* value);
-
 struct Queue {
 	Node* Head, * Tail;
-	void init() {Head = Tail =  nullptr;}
-	bool isEmpty() {return Head == nullptr;}
-	void createQueue(int size, FILE* file, const char* fileName) {;
-		void* value = nullptr;
-		while (size--) {
-			value = readfile(file, fileName);
-			push(value);
-		}
-	}
-	void push(void* value) {
-		if (value == nullptr) {showNotification("Du lieu khong hop le\n");}
-		Node *newNode = new Node(value);
-		isEmpty() ? Head = newNode : Tail->next = newNode;
-		Tail = newNode;
-	}
-	void* pop() {
-		if (isEmpty()) {return nullptr;}
-		Node* temp = Head;
-		void* value = temp->data;
-		Head = Head->next;
-		delete temp;
-		temp = nullptr;
-		return value;
-	}
-	void* deleteAfter(Node* current) {
-		if (current == nullptr) {
-			showNotification("\nNode khong ton tai do kich thuoc danh sach khong phu hop!");
-			exit(0);
-		};
-		if (current->next == Tail) Tail = current;
-		void* value = current->next->data;
-		Node* next_next = current->next->next;
-		delete current->next;
-		current->next = next_next;
-		return value;
-	}
-	void printQueue(const char* type) {
-		int count = 0;
-		if (Head == nullptr) 
-			showNotification("\nHang doi rong");
-		Node* current = Head;
-		while (current != nullptr) {
-			printf("\n - %s %02d:\n", type, ++count);
-			if (!strcmp(type, "DON THUOC"))    {printPrescription ((Don_thuoc*)    current->data);}
-			if (!strcmp(type, "HOA DON NHAP")) {printImportingBill((Hoa_Don_Nhap*) current->data);}
-			if (!strcmp(type, "HOA DON XUAT")) {printExportingBill((Hoa_Don_Xuat*) current->data);}
-			current =  current->next;
-		}
-	}
-	Node* getHead() {return Head;}
+	void init();
+	bool isEmpty();
+	void push(void* value);
+	void* pop();
+	void create(int size, FILE* file, const char* fileName);
+	void print(const char* type);
+	Node* getHead();
 };
-// Menu function //
-void Menu(int start, int end);
+struct List: public Queue {
+	void* deleteAfter(Node* current);
+}; 
+
+// NHOM HAM THONG BAO  :
+void showNotification(const char* notification);
+
+// NHOM HAM DOC FILE   :
+void* readfile(FILE* file, const char* fileName);
+
+// NHOM HAM LAY DU LIEU:
+/*======================================================================================*/
+/* + DON THUOC - HOA DON NHAP - HOA DON XUAT*/
+void getProperties(FILE* file, void* variable, const char* format, int vType, int pType);
+void* getPrescription(FILE* file);
+Hoa_Don getBill(FILE* file);
+void* getImportingBill(FILE* file);
+void* getExportingBill(FILE* file);
+/*======================================================================================*/
+/* + NGAY THANG NAM */
+void getDate(FILE* file, const char* format, Date& variable);
+Date getCurrentDate();
+/*======================================================================================*/
+/* + MENU */
+void* getMaxOrMin(const char* type, Queue EXP_BiLL_Queue);
 void getChoice(int &choosen, int minCase, int maxCase);
-void KHOI_TAO_DS_DON_THUOC(Queue& Prescription_Queue, FILE* Don_thuoc);
+/*======================================================================================*/
+// NHOM HAM XUAT DU LIEU:
+void printPrescription(Don_thuoc* value);
+void printBill(Hoa_Don value, const char* string);
+void printExportingBill(Hoa_Don_Xuat* value);
+void printImportingBill(Hoa_Don_Nhap* value);
+/*======================================================================================*/
+// NHOM HAM KHAC       :
+bool isValidDate(struct Date date);
+bool checkExpiry(Date end);
+int checkMonth(Date end, int numbers, int type);
+int daysInMonth(int month, int year);
+/*======================================================================================*/
+// CHUC NANG CHINH CUA CHUONG TRINH
+void Menu(int start, int end);
+void KHOI_TAO_DS_DON_THUOC(List& Prescription_List, FILE* Don_thuoc);
 void LAP_HOA_DON(Queue& IMP_BiLL_Queue, Queue& EXP_BiLL_Queue, FILE* Hoa_Don_Nhap, FILE* Hoa_Don_Xuat);
 void LAP_HOA_DON_NHAP(Queue& IMP_BiLL_Queue, FILE* Hoa_Don_Nhap);
 void LAP_HOA_DON_XUAT(Queue& EXP_BiLL_Queue, FILE* Hoa_Don_Xuat);
-void TIM_KIEM_DON_THUOC(Queue Prescription_Queue, Queue IMP_BiLL_Queue, Queue EXP_BiLL_Queue);
-void TIM_DON_THUOC_THEO_HOA_DON(Queue Prescription_Queue, Queue IMP_BiLL_Queue, Queue EXP_BiLL_Queue);
+void TIM_KIEM_DON_THUOC(List Prescription_List, Queue IMP_BiLL_Queue, Queue EXP_BiLL_Queue);
+void TIM_DON_THUOC_THEO_HOA_DON(List Prescription_List, Queue IMP_BiLL_Queue, Queue EXP_BiLL_Queue);
 void IN_DANH_SACH_HOA_DON(Queue IMP_BiLL_Queue, Queue EXP_BiLL_Queue);
 void TIM_THONG_TIN_DON_THUOC(Queue IMP_BiLL_Queue, Queue EXP_BiLL_Queue);
 void THONG_TIN_DON_THUOC_BAN_NHIEU_NHAT_IT_NHAT(Queue EXP_BiLL_Queue, const char* type);
-void* getMaxOrMin(const char* type, Queue EXP_BiLL_Queue);
 void THONG_TIN_DON_THUOC_CO_THOI_GIAN_SD_DUOI_6_THANG(Queue IMP_BiLL_Queue) ;
-int daysInMonth(int month, int year);
-bool isValidDate(struct Date date);
-bool checkExpiry(Date end);
-Date getCurrentDate();
-int checkMonth(Date end, int numbers);
 void IN_RA_THONG_TIN_CAC_DON_THUOC_TON_SLG_LON_HON_N(int number, Queue IMP_BiLL_Queue, Queue EXP_BiLL_Queue);
 void THONG_KE_THUOC_CO_THOI_HAN_SU_DUNG_DUOI_30_NGAY(Queue IMP_BiLL_Queue);
-void XOA_DON_THUOC(Queue Prescription_Queue, Queue IMP_BiLL_Queue, Queue EXP_BiLL_Queue);
+void XOA_DON_THUOC(List& Prescription_List, Queue& IMP_BiLL_Queue);
+
 #endif index_h
